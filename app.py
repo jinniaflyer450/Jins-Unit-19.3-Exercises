83a274df634869e03bf9e14de642a8859b7d703f
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-06-25 13:56:51 -0500

    finished assignment

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 """Allows users to take a satisfaction survey and stores their responses for later use."""
 from surveys import Question, Survey, satisfaction_survey
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
 app=Flask(__name__)
+app.config['SECRET_KEY'] = 'catdog'
+debug = DebugToolbarExtension(app)
 
 max_question_number = len(satisfaction_survey.questions)
 question_number = 0
@@ -18,24 +21,36 @@ def survey_start():
 def survey_question(question_number):
     option_number = 0
     options_and_indices = {}
-    if question_number + 1 <= max_question_number:
+    if question_number + 1 <= max_question_number and question_number == len(responses):
         survey_question = satisfaction_survey.questions[question_number].question
         survey_options=satisfaction_survey.questions[question_number].choices
         for option in survey_options:
             options_and_indices[option] = option_number
             option_number += 1
-    question_number+= 1
-    if question_number > max_question_number:
-        return redirect('/end_survey')
-    else:
         return render_template('survey_question.html', question_number=question_number, max_question_number=max_question_number, survey_question=survey_question, options_and_indices=options_and_indices)
+    elif len(responses) == max_question_number:
+        flash(f"You have already completed the survey!")
+        return redirect('/end_survey')
+    elif question_number + 1 > max_question_number and len(responses) < max_question_number:
+        flash(f"The survey does not have question {question_number}!")
+        return redirect(f'/questions/{len(responses)}')
+    else:
+        question_number = len(responses)
+        flash("Please do the questions in order!")
+        return redirect(f'/questions/{question_number}')
 
 @app.route('/answer', methods = ['POST'])
 def get_answers():
-    answer = request.form[f'question_{question_number + 1}']
-    responses.append(satisfaction_survey.questions[question_number].choices[int(answer)])
-    return redirect(f'/questions/{question_number + 1}')
+    global question_number
+    answer = request.form[f'question_{question_number}']
+    question_number += 1
+    if question_number < max_question_number:
+        responses.append(satisfaction_survey.questions[question_number - 1].choices[int(answer)])
+        return redirect(f'/questions/{question_number}')
+    elif question_number == max_question_number:
+        responses.append(satisfaction_survey.questions[question_number - 1].choices[int(answer)])
+        return redirect('/end_survey')
 
 @app.route('/end_survey')
 def survey_end():
-    return render_template('end_survey.html')
+    return render_template('end_survey.html', responses=responses)
